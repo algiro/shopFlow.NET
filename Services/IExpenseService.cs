e050257d578c30ecb293de897147e6a7d106aca4
AleG <ag@audacitycapital.com>
2025-03-03 20:12:41 +0000

    Persistency: create IExpensesPersistency interface

--- Services/IExpenseService.cs
+++ Services/IExpenseService.cs
@@ -1,54 +1,55 @@
 using shopFlow.Persistency;
 
-namespace shopFlow.Services {
+namespace shopFlow.Services
+{
 
-    public interface IExpenseService {
+    public interface IExpenseService
+    {
         bool Add(IExpense expense);
         bool Delete(IExpense expense);
         bool Update(IExpense expense);
-        void Upgrade();
-        IEnumerable<IExpense> GetExpenses((int Year,int Month) period);
-        IEnumerable<IExpense> GetExpenses(DateOnly fromDate,DateOnly toDate);
+        IEnumerable<IExpense> GetExpenses(DateOnly fromDate, DateOnly toDate);
 
     }
 
-    public class DefaultExpenseService : IExpenseService
+    public class DefaultExpenseService(IExpensesPersistency expensesPersistency) : IExpenseService
     {
-        public bool Add(IExpense expense) {
-            try {
-                return ExpensesPersistency.Save(expense);
+        public bool Add(IExpense expense)
+        {
+            try
+            {
+                return expensesPersistency.Save(expense);
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 Console.Error.WriteLine("Error on IExpenseService.Add " + expense + " error:" + ex);
                 return false;
             }
         }
-        public bool Delete(IExpense expense) {
-            try {
-                return ExpensesPersistency.Delete(expense);
+        public bool Delete(IExpense expense)
+        {
+            try
+            {
+                return expensesPersistency.Delete(expense);
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 Console.Error.WriteLine("Error on IExpenseService.Add " + expense + " error:" + ex);
                 return false;
             }
         }
-        public bool Update(IExpense expense) {
-            try {
-                return ExpensesPersistency.Save(expense);
+        public bool Update(IExpense expense)
+        {
+            try
+            {
+                return expensesPersistency.Save(expense);
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 Console.Error.WriteLine("Error on IExpenseService.Add " + expense + " error:" + ex);
                 return false;
             }
-        }        
-        public IEnumerable<IExpense> GetExpenses((int Year,int Month) period)  => ExpensesPersistency.LoadExpenses(period);
-        public IEnumerable<IExpense> GetExpenses(DateOnly fromDate,DateOnly toDate) => ExpensesPersistency.LoadExpenses(fromDate,toDate);
-
-        public void Upgrade() {
-            var oldExpenses = ExpensesPersistency.LoadExpenses(new DateOnly(2024,05,01),new DateOnly(2025,01,30));
-            foreach (var exp in oldExpenses) {
-                Update(exp);
-            }
         }
+        public IEnumerable<IExpense> GetExpenses(DateOnly fromDate, DateOnly toDate) => expensesPersistency.LoadExpenses(fromDate, toDate);
     }
 }
