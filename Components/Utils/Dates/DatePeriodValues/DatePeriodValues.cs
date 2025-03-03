0c14889744eb58adc53f4d155c04f6d53cfc678e
AleG <ag@audacitycapital.com>
2025-03-03 16:31:58 +0000

    Components\Pages: minor fixes to remove warnings during build

---
+++ Components/Utils/Dates/DatePeriodValues/DatePeriodValues.cs
@@ -0,0 +1,9 @@
+namespace shopFlow.Utils.Dates
+{
+    // Class to hold the date values
+    public class DatePeriodValues
+    {
+        public DateOnly FromDate { get; set; }
+        public DateOnly ToDate { get; set; }
+    }
+}
--- Config/ShopFlowConfig.cs
+++ Config/ShopFlowConfig.cs
@@ -6,12 +6,12 @@ namespace shopFlow.Config
     public interface IShopFlowConfig
     {
         string PersistencyMainFolder { get; }
-        string[] Supplies { get; }
-        string[] ExpensesTypes { get; }
+        string[] GetSupplies();
+        string[] GetExpensesTypes();
     }
     public class ShopFlowConfig
     {
-        public static IShopFlowConfig Instance = new DefaultShopFlowConfig();
+        public static readonly IShopFlowConfig Instance = new DefaultShopFlowConfig();
         public static void SetConfigurationManger(ConfigurationManager configuration)
         {
             _configurationManager = configuration;
@@ -19,9 +19,9 @@ namespace shopFlow.Config
         private static ConfigurationManager? _configurationManager;
         private sealed class DefaultShopFlowConfig : IShopFlowConfig
         {
-            public string PersistencyMainFolder { get; } = "/data/shopFlowMovs"; // default volume target
-            public string[] Supplies => ExpensesConfigPersistency.LoadSupplies().ToArray();
-            public string[] ExpensesTypes => ExpensesConfigPersistency.LoadExpTypes().ToArray();
+            public string PersistencyMainFolder { get; } = _configurationManager?.GetValue<string>("PersistencyMainFolder") ?? "/data/shopFlowMovs";
+            public string[] GetSupplies() => ExpensesConfigPersistency.LoadSupplies().ToArray();
+            public string[] GetExpensesTypes() => ExpensesConfigPersistency.LoadExpTypes().ToArray();
         }
     }
 }
