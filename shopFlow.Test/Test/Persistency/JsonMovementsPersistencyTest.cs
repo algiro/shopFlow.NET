972baed54d6d4b6efe3b1983179fdcc3383727f2
AleG <ag@audacitycapital.com>
2025-03-05 17:41:42 +0000

    Test\Persistency\JsonMovementsPersistencyTest: improved TCs

--- shopFlow.Test/Test/Persistency/JsonMovementsPersistencyTest.cs
+++ shopFlow.Test/Test/Persistency/JsonMovementsPersistencyTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using shopFlow.Persistency;
 using shopFlow.Services;
 using System;
@@ -13,7 +14,7 @@ namespace showFlow.Test.Test.Persistency
 {
     public class JsonMovementsPersistencyTest
     {
-        private IFileSystem _fileSystem;
+        private IFileSystem? _fileSystem;
         [SetUp]
         public void SetUp()
         {
@@ -23,44 +24,60 @@ namespace showFlow.Test.Test.Persistency
                     { @"/data/shopFlowMovs/Movements/2025-01/2025-01-02_18-02-24_Open_Drawer_MOV.json", new MockFileData("{\"Date\":\"2025-01-02T18:02:24.8632138\",\"Amount\":9.0,\"Type\":\"Open\",\"Source\":\"Drawer\"}") },
                     { @"/data/shopFlowMovs/Movements/2025-02/2025-02-01_15-02-24_Open_Drawer_MOV.json", new MockFileData("{\"Date\":\"2025-02-01T15:02:24.8632138\",\"Amount\":8.0,\"Type\":\"Open\",\"Source\":\"Drawer\"}") },
                 });
-
         }
+
+        [Test]
+        public void LoadMovements_NoMatch()
+        {
+            JsonMovementPersistency jsonMovementPersistency = new JsonMovementPersistency(_fileSystem!);
+            var movements = jsonMovementPersistency.LoadMovements(new DateOnly(2024, 01, 01), new DateOnly(2024, 01, 02));
+            movements.Count().Should().Be(0);
+        }
+
 
         [Test]
         public void LoadMovements_SamePeriod_Success()
         {
-            JsonMovementPersistency jsonMovementPersistency = new JsonMovementPersistency(_fileSystem);
+            JsonMovementPersistency jsonMovementPersistency = new JsonMovementPersistency(_fileSystem!);
             var movements = jsonMovementPersistency.LoadMovements(new DateOnly(2025,01,01), new DateOnly(2025, 01, 02));
-            Assert.That(movements.Count(), Is.EqualTo(2));  
+            movements.Count().Should().Be(2);  
         }
-        
+
         [Test]
         public void LoadMovements_DifferentPeriods_Success()
         {
-            JsonMovementPersistency jsonMovementPersistency = new JsonMovementPersistency(_fileSystem);
+            JsonMovementPersistency jsonMovementPersistency = new JsonMovementPersistency(_fileSystem!);
             var movements = jsonMovementPersistency.LoadMovements(new DateOnly(2025, 01, 01), new DateOnly(2025, 02, 02));
-            Assert.That(movements.Count(), Is.EqualTo(3));
+            movements.Count().Should().Be(3);
         }
 
         [Test]
         public void Save_VerifyItsPresent_Success()
         {
-            JsonMovementPersistency jsonMovementPersistency = new JsonMovementPersistency(_fileSystem);
+            JsonMovementPersistency jsonMovementPersistency = new JsonMovementPersistency(_fileSystem!);
             jsonMovementPersistency.Save(Movement.Create(new DateTime(2025, 01, 03), 6.4m, MovementType.Deposit, SourceType.Card));
             var movements = jsonMovementPersistency.LoadMovements(new DateOnly(2025, 01, 01), new DateOnly(2025, 01, 03));
-            Assert.That(movements.Count(), Is.EqualTo(3));
+            movements.Count().Should().Be(3);
         }
 
         [Test]
         public void Delete_VerifyDeletion_Success()
         {
-            JsonMovementPersistency jsonMovementPersistency = new JsonMovementPersistency(_fileSystem);
+            JsonMovementPersistency jsonMovementPersistency = new JsonMovementPersistency(_fileSystem!);
             var movements = jsonMovementPersistency.LoadMovements(new DateOnly(2025, 01, 01), new DateOnly(2025, 01, 01));
-            Assert.That(movements.Count(), Is.EqualTo(1));
-            jsonMovementPersistency.Delete(movements.First());
+            movements.Count().Should().Be(1);
+            jsonMovementPersistency.Delete(movements.First()).Should().BeTrue();
             var movementsAfterDelete = jsonMovementPersistency.LoadMovements(new DateOnly(2025, 01, 01), new DateOnly(2025, 01, 01));
-            Assert.That(movementsAfterDelete.Count(), Is.EqualTo(0));
-
+            movementsAfterDelete.Count().Should().Be(0);
         }
+
+        [Test]
+        public void Delete_NotMatchingMovement_Success()
+        {
+            JsonMovementPersistency jsonMovementPersistency = new JsonMovementPersistency(_fileSystem!);
+            var notExistingMovement = Movement.Create(new DateTime(2024, 01, 01), 6.4m, MovementType.Deposit, SourceType.Card);            
+            jsonMovementPersistency.Delete(notExistingMovement).Should().BeFalse();
+        }
+
     }
 }
