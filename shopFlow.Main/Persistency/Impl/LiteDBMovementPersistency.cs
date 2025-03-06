8f0ae4c4682f5640a9142fe10521a2b58cae0105
AleG <ag@audacitycapital.com>
2025-03-06 14:00:35 +0000

    shopFlow.Main\Persistency\Impl: added implementation for LiteDB

---
+++ shopFlow.Main/Persistency/Impl/LiteDBMovementPersistency.cs
@@ -0,0 +1,56 @@
+﻿using LiteDB;
+using shopFlow.Components.Pages;
+using shopFlow.Services;
+using shopFlow.Utils;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace shopFlow.Persistency.Impl
+{
+    public class LiteDBMovementPersistency : IMovementPersistency
+    {
+        private readonly TimeOnly DAY_TIME_START = new TimeOnly(0, 0, 0);
+        private readonly TimeOnly DAY_TIME_END = new TimeOnly(23, 59, 59);
+        private readonly ILogger _logger = LoggerUtils.CreateLogger<LiteDBMovementPersistency>();
+        private readonly LiteDatabase _db;
+        private const string MOVEMENTS_COLLECTION = "movements";
+        public LiteDBMovementPersistency(LiteDatabase db)
+        {
+            _db = db;
+            _db.GetCollection<DefaultMovement>(MOVEMENTS_COLLECTION).EnsureIndex(m => m.Id,unique:true);
+            _db.GetCollection<DefaultMovement>(MOVEMENTS_COLLECTION).EnsureIndex(m => m.Date);
+        }
+        public bool Delete(IMovement movement)
+        {
+            var hasBeenDeleted = _db.GetCollection<DefaultMovement>(MOVEMENTS_COLLECTION).Delete(movement.Id);
+            _logger.LogInformation("Delete movement: {Movement} deleted:{HasBeenDeleted}", movement, hasBeenDeleted);
+            return hasBeenDeleted;
+        }
+
+        public IEnumerable<IMovement> LoadMovements(DateOnly fromDate, DateOnly toDate)
+        {
+            _logger.LogInformation("LoadMovements movement: {FromDate} to {ToDate}", fromDate, toDate);
+            var movements = _db.GetCollection<DefaultMovement>(MOVEMENTS_COLLECTION).Query().Where(m => m.Date >= fromDate.ToDateTime(DAY_TIME_START) && m.Date <= toDate.ToDateTime(DAY_TIME_END)).ToList();
+            _logger.LogInformation("LoadMovements movement: {FromDate} to {ToDate} loaded#:{MovCount}", fromDate, toDate, movements.Count);
+            return movements;
+        }
+
+        public bool Save(IMovement movement)
+        {
+            _logger.LogInformation("Save movement: {Movement}", movement);
+            var movColl = _db.GetCollection<DefaultMovement>(MOVEMENTS_COLLECTION);
+            var storedMovement = movColl.FindById(movement.Id);
+            if (storedMovement != null)
+            {
+                var hasBeenUpdated = movColl.Update(movement.AsDefaultMovement());
+                _logger.LogInformation("Update movement: {Movement} hasBeenUpdated:{hasBeenUpdated}", movement, hasBeenUpdated);
+                return true;
+            }
+            else
+            {
+                var idMov = movColl.Insert(movement.AsDefaultMovement());
+                _logger.LogInformation("Insert movement: {Movement} with Id:{Id}", movement, idMov);
+                return true;
+            }
+        }
+    }
+}
---
+++ shopFlow.Test/Test/Persistency/MigrationMovsTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using shopFlow.Persistency;
+using shopFlow.Persistency.Impl;
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+using FluentAssertions;
+
+namespace shopFlow.Test.Test.Persistency
+{
+    public class MigrationMovsTest
+    {
+        private IFileSystem? _fileSystem;
+        private LiteDatabase? _db;
+        [SetUp]
+        public void SetUp()
+        {
+            _fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+                {
+                    { @"/data/shopFlowMovs/Movements/2025-01/2025-01-01_20-02-24_Open_Drawer_MOV.json", new MockFileData("{\"Date\":\"2025-01-01T20:02:24.8632138\",\"Amount\":10.0,\"Type\":\"Open\",\"Source\":\"Drawer\"}") },
+                    { @"/data/shopFlowMovs/Movements/2025-01/2025-01-02_18-02-24_Open_Drawer_MOV.json", new MockFileData("{\"Date\":\"2025-01-02T18:02:24.8632138\",\"Amount\":9.0,\"Type\":\"Open\",\"Source\":\"Drawer\"}") },
+                    { @"/data/shopFlowMovs/Movements/2025-02/2025-02-01_15-02-24_Open_Drawer_MOV.json", new MockFileData("{\"Date\":\"2025-02-01T15:02:24.8632138\",\"Amount\":8.0,\"Type\":\"Open\",\"Source\":\"Drawer\"}") },
+                });
+            MemoryStream memoryStream = new MemoryStream();
+            _db = new LiteDatabase(memoryStream);
+        }
+
+        [Test]
+        public void Start_FromJsonToLiteDB_Success()
+        {
+            var source = new JsonMovementPersistency(_fileSystem!);
+            var dest = new LiteDBMovementPersistency(_db!);
+            var migration = new MigrationMovs(source, dest);
+            migration.Start(new DateOnly(2024, 01, 01),new DateOnly(2026,01,01));
+            var destMovements = dest.LoadMovements(new DateOnly(2025, 01, 01), new DateOnly(2025, 02, 01));
+            destMovements.Count().Should().Be(3);
+            var sourceMovements = source.LoadMovements(new DateOnly(2025, 01, 01), new DateOnly(2025, 02, 01));
+            sourceMovements.Count().Should().Be(3);
+            foreach (var sourceMov in sourceMovements)
+            {
+                sourceMov.Equals(destMovements.Single(d => d.Id == sourceMov.Id)).Should().BeTrue();
+            }
+
+        }
+    }
+}
